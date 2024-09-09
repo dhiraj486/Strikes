8f31ef23555a4c01440732262d11bf6000b6015a
Dhiraj T H <91175366+dhiraj486@users.noreply.github.com>
2024-09-09 19:58:44 +0530

    twosum.py
    The file explains the problem-solving method for the two sum method using a class method.

---
+++ twosum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j] == target:
+                    new_list = i,j
+                    return new_list
+        return -1
+solution = Solution()
